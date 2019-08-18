a3621a09e2b99790a14096cb299b63d72b09e876
Adamaya <mail.adamyas@gmail.com>
2019-08-18 14:49:38 +0530

    adding new code

--- src/Practice_1.java
+++ src/Practice_1.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Practice_1 {
+    public static void main(String[] args) {
+        /*
+        //Solutio of Question 1
+        Scanner input= new Scanner(System.in);
+        String name=input.nextLine();
+        input.close();
+        System.out.println("Welcome, "+name+"!");
+
+         */
+        //Solution of Question 2
+        /*
+        Scanner input= new Scanner(System.in);
+        String name=input.nextLine();
+        input.close();
+        String[] list=name.split(" ",5);
+        System.out.println(list[0]+" University "+list[1]);
+
+         */
+        //Solution of Question 3
+        /*
+        Scanner input= new Scanner(System.in);
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        int sum=num1+num2;
+        int sub=num1-num2;
+        int mul=num1*num2;
+        int dev=num1/num2;
+        System.out.println(num1+"+"+num2+"="+sum);
+        System.out.println(num1+"-"+num2+"="+sub);
+        System.out.println(num1+"*"+num2+"="+mul);
+        System.out.println(num1+"/"+num2+"="+dev);
+        input.close();
+
+         */
+        //Solution of Question 4
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        input.close();
+        
+    }
 }
